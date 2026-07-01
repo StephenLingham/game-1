0f67936171c89a71e5efae74babcfe8e5911d81c
Stephen Lingham <147378+StephenLingham@users.noreply.github.com>
2026-07-01 22:14:44 +0100

    Changed boss health from 50k down to 30k

--- scripts/game_constants.gd
+++ scripts/game_constants.gd
@@ -112,7 +112,7 @@ const ENEMY_GOLEM_XP_MAX: int = 120
 
 const ENEMY_BOSS_SPRITE_SCALE: float = 0.22
 const ENEMY_BOSS_SPEED: float = 45.0
-const ENEMY_BOSS_HEALTH: int = 50000
+const ENEMY_BOSS_HEALTH: int = 30000
 const ENEMY_BOSS_DAMAGE: int = 55
 const ENEMY_BOSS_ATTACK_COOLDOWN: float = 1.2
 const ENEMY_BOSS_XP_MIN: int = 250
